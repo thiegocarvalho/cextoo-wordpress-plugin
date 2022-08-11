02cd49504c6d902b0246bcc4f575784a5338cf4b
ThiegoCarvalho <carvalho.thiego@gmail.com>
2022-08-10 23:32:56 -0300

    fix css

--- public/partials/cextoo-subscriptions-table.php
+++ public/partials/cextoo-subscriptions-table.php
@@ -11,33 +11,36 @@
 }
 
 .cextoo-card {
-    background: rgba(255, 255, 255, 0.45);
+    background-color: #F5F5F5;
     border-radius: 16px;
+    border: .5px solid #E0E0E0;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-left: 12px solid #00a0d2;
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(5px);
-    -webkit-backdrop-filter: blur(5px);
     padding: .8rem;
     margin: .5rem;
 }
 
 .cextoo-card-title {
+    color: #708090;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
     font-size: .9rem;
     font-weight: bold;
 }
 
 .cextoo-card-text {
-    color: #383838;
+    color: #708090;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
     font-size: .7rem;
 }
 
 .cextoo-button {
     width: 90px;
-    font-size: 12px;
+    height: 32px;
     font-weight: 400;
     color: #fff;
     cursor: pointer;
-    margin: 10px;
-    height: 30px;
     text-align: center;
     border: none;
     background-size: 300% 100%;
@@ -79,7 +82,7 @@
 <div class="cextoo-cards">
     <?php foreach ($subscriptions as $subscription) :
         if ($subscription->status) {
-            $status = '🟢';
+            $status = 'border-left-color: #90EE90;';
             $button = [
                 'text' => "cancelar",
                 'class' => 'cextoo-button bg-cancel',
@@ -87,37 +90,44 @@
                 'action' => 'https://checkout.defiverso.com/subsciption/' . $subscription->external_id
             ];
         } else {
-            $status = '🔴';
+            $status = 'border-left-color: #999999;';
             $button = [
-                'text' => "<s>cancelar<s>",
+                'text' => "<s>cancelar</s>",
                 'class' => 'cextoo-button bg-desactivate',
                 'action' => false
             ];
         }
     ?>
-    <div class="cextoo-card">
+    <div class="cextoo-card" style="<?php echo $status; ?>">
         <div class="cextoo-card-body">
             <p class="cextoo-card-title">
-                <span><?php echo $status ?></span>
-                <span><?php echo $subscription->product_name ?>
+                <?php echo $subscription->product_name ?>
             </p>
-            <p class="cextoo-card-text"><strong>Inicio: </strong>
+            <p class="cextoo-card-text"><strong>adquirido: </strong>
                 <?php echo date("d/m/Y", strtotime($subscription->start_at)) ?>
             </p>
 
-            <?php if ($subscription->renew_at) : ?>
+
             <p class="cextoo-card-text">
-                <strong>Renovação: </strong><?php echo date("d/m/Y", strtotime($subscription->renew_at)) ?>
+                <?php if ($subscription->renew_at) : ?>
+                <strong>renovação: </strong>
+                <?php echo date("d/m/Y", strtotime($subscription->renew_at)) ?>
+                <?php else : ?>
+                --
+                <?php endif; ?>
             </p>
-            <?php endif; ?>
 
-            <?php if ($subscription->expires_at) : ?>
-            <p class="cextoo-card-text"><strong>Cancelamento: </strong>
+            <p class="cextoo-card-text">
+                <?php if ($subscription->expires_at) : ?>
+                <strong>cancelado: </strong>
                 <?php echo date("d/m/Y", strtotime($subscription->expires_at)) ?>
+                <?php else : ?>
+                --
+                <?php endif; ?>
             </p>
-            <?php endif; ?>
 
-            <p class="cextoo-card-text" style="text-align: center;">
+
+            <p class="cextoo-card-text">
                 <?php if ($button['action']) : ?>
                 <a href="<?php echo $button['action']; ?>" target="_blank">
                     <button class="<?php echo $button['class']; ?>">
